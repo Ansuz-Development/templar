2f0bc3839d0032ace72a99d651a93eb6f6dbcea7
AnsuzDev <ansuzdev@gmail.com>
2022-06-02 13:36:32 +0700

    fix item schema

--- test.js
+++ test.js
@@ -10,10 +10,16 @@ import * as XLSX from "xlsx/xlsx.mjs";
 const __dirname = path.resolve();
 
 // Load the docx file as binary content
-const content = fs.readFileSync(
-  path.resolve(__dirname, "example.docx"),
-  "binary"
-);
+const contents = [
+  fs.readFileSync(
+    path.resolve(__dirname, "example.docx"),
+    "binary"
+  ),
+  fs.readFileSync(
+    path.resolve(__dirname, "example_2.docx"),
+    "binary"
+  ),
+];
 
 const buf = readFileSync("./example.xlsx");
 /* buf is a Buffer */
@@ -22,25 +28,27 @@ const workbook = XLSX.read(buf);
 var sheet_name_list = workbook.SheetNames;
 const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
-for (const person of data) {
-  console.log(person);
+for (const content of contents) {
+  for (const person of data) {
+    console.log(person);
 
-  const zip = new PizZip(content);
-  const doc = new Docxtemplater(zip, {
-    paragraphLoop: true,
-    linebreaks: true,
-  });
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, {
+      paragraphLoop: true,
+      linebreaks: true,
+    });
 
-  doc.render(person);
+    doc.render(person);
 
-  const docxBuf = doc.getZip().generate({
-      type: "nodebuffer",
-      // compression: DEFLATE adds a compression step.
-      // For a 50MB output document, expect 500ms additional CPU time
-      compression: "DEFLATE",
-  });
+    const docxBuf = doc.getZip().generate({
+        type: "nodebuffer",
+        // compression: DEFLATE adds a compression step.
+        // For a 50MB output document, expect 500ms additional CPU time
+        compression: "DEFLATE",
+    });
 
-  // buf is a nodejs Buffer, you can either write it to a
-  // file or res.send it with express for example.
-  fs.writeFileSync(path.resolve(__dirname, `output-${person.name}.docx`), docxBuf);
+    // buf is a nodejs Buffer, you can either write it to a
+    // file or res.send it with express for example.
+    fs.writeFileSync(path.resolve(__dirname, `${new Date().getTime()}-output-${person.name}.docx`), docxBuf);
+  }
 }
